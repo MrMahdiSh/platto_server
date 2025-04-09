fc880bbfedd2a1ff64268da6bb1d66d08ae34250
Mahdi <mahdishoorabi@gmail.com>
2025-04-09 18:31:38 +0330

    prepare for server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const WebSocket = require("ws");
 const authRoutes = require("./routes/authRoutes");
@@ -11,21 +12,43 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-const server = http.createServer(app);
+
+const isProduction = process.env.NODE_ENV === "production"; // Check if in production
+
+// If in production, use HTTPS with certificates, otherwise use HTTP with port 3000
+let server;
+
+if (isProduction) {
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/ruform.ir/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/ruform.ir/cert.pem"),
+    ca: fs.readFileSync("/etc/letsencrypt/live/ruform.ir/chain.pem"),
+  };
+
+  // Create an HTTPS server for production
+  server = https.createServer(options, app).listen(443, () => {
+    console.log("Server is running on https://ruform.ir");
+  });
+} else {
+  // Create an HTTP server for development/test (localhost)
+  server = app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
+}
+
 const wss = new WebSocket.Server({ server });
 
 app.use(express.json());
 
 app.use(authRoutes);
-
 app.use(gameRoute);
-
 app.use(errorMiddleware);
 
 require("./sockets/gameSocket")(wss);
 
 console.log(process.env.MONGODB_URI);
-// Your MongoDB connection here
+
+// MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -37,8 +60,3 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
-// Start server
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
